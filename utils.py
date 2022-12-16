1cfa319e983d6e2dc5f1d8650329640f1aa99924
Adam Ksiezyk <adam.ksiezyk@nokia.com>
2022-12-16 13:53:53 +0100

    Lab 7

--- utils.py
+++ utils.py
@@ -1,12 +1,17 @@
+import time
 import pandas as pd
 import os
 import re
+import requests
 from typing import Any, Callable
+from xml.etree import ElementTree as ET
 
 
 Processor = Callable[[str], Any]
 
 FILES_PATH = "ustawy"
+CLARIN_URL = "https://ws.clarin-pl.eu/nlprest2/base/"
+CLARIN_USER = "adamksiezyk@student.agh.edu.pl"
 
 
 def read_documents(path: str = FILES_PATH) -> dict[str, str]:
@@ -47,3 +52,54 @@ def _normalize_document(document: str) -> str:
 
 def _process_document(document: str, *processors: Processor) -> list:
     return [p(document) for p in processors]
+
+
+def parse_analysis(xml_str: str) -> list[str]:
+    tags = []
+    root = ET.fromstring(xml_str)
+    for lex in root.findall("./chunk/sentence/tok"):
+        base = lex.find("lex/base").text
+        ctag = lex.find("lex/ctag").text.split(':')[0]
+        tags.append(f"{base}:{ctag}")
+    return tags
+
+
+def get_base_text(xml_str: str) -> str:
+    tags = []
+    root = ET.fromstring(xml_str)
+    for lex in root.findall("./chunk/sentence/tok"):
+        tags.append(lex.find("lex/base").text)
+    return ' '.join(tags)
+
+
+def clarine(text: str, lpmn: str, user: str = CLARIN_USER) -> str:
+    res_upload = requests.post(f"{CLARIN_URL}/upload",
+                               data=text.encode('utf-8'),
+                               headers={'Content-Type': "binary/octet-stream"})
+    res_upload.raise_for_status()
+    file_id = res_upload.text
+
+    res_task = requests.post(f"{CLARIN_URL}/startTask",
+                             json={
+                                 'user': user,
+                                 'lpmn': lpmn,
+                                 'file': file_id
+                             })
+    res_task.raise_for_status()
+    task_id = res_task.text
+
+    while True:
+        res_status = requests.get(f"{CLARIN_URL}/getStatus/{task_id}")
+        res_status.raise_for_status()
+        res_status_json = res_status.json()
+        print(res_status_json)
+        if res_status_json['status'] == "ERROR":
+            raise RuntimeError(res_status_json)
+        if res_status_json['status'] == "DONE":
+            break
+        time.sleep(5)
+    output_file_id = res_status_json['value'][0]['fileID']
+
+    res_download = requests.get(f"{CLARIN_URL}/download{output_file_id}")
+    res_download.raise_for_status()
+    return res_download.text
